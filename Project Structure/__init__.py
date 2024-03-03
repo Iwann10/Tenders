b7b49a1887af991a1430b4943b7fb3a2fa6877cb
Iwann10 <111977835+Iwann10@users.noreply.github.com>
2024-03-03 14:09:23 +0200

    init

---
+++ Project Structure/__init__.py
@@ -0,0 +1,16 @@
+# __init__.py
+
+# You can add initialization code here if needed
+# This file is required to treat the directory as a Python package
+# It can be left empty if no initialization is needed
+
+# Example: Importing modules from the package
+# from .modules import module1, module2
+# from .tests import test_module1, test_module2
+
+from .NGramAnalysis import ngram_analysis
+from .PolySyllabic_Words import polysyllabic_words
+from .Readability import readability
+from .Sentiment_Analysis import sentiment_analysis
+
+from your_package import ngram_analysis, polysyllabic_words, readability, sentiment_analysis
